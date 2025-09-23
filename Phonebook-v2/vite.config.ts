f133ce5cfc016f57d12b62cc19c43449dd912f66
Anthony Osunde <Anosunde@buagroup.local>
2025-09-23 11:52:17 +0100

    fix vite config

--- Phonebook-v2/vite.config.ts
+++ Phonebook-v2/vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // This allows connections from all IP addresses
+    port: 5173,      // Your development port
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
